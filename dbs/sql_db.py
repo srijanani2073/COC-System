639ce4b75ee8ac824d54a805392170398220dbef
srijanani2073 <151195238+srijanani2073@users.noreply.github.com>
2026-04-25 19:06:32 +0530

    Update sql_db.py

--- dbs/sql_db.py
+++ dbs/sql_db.py
@@ -1,11 +1,11 @@
 import psycopg2
 
 def get_connection():
-        host = os.environ.get("PG_HOST")
-        db   = os.environ.get("PG_DB")
-        user = os.environ.get("PG_USER")
-        pwd  = os.environ.get("PG_PASSWORD")
-        port = os.environ.get("PG_PORT", "5432")
+        host = os.environ.get("host")
+        db   = os.environ.get("db")
+        user = os.environ.get("user")
+        pwd  = os.environ.get("pwd")
+        port = os.environ.get("port", "5432")
         sslmode="require"
 
         if not all([host, db, user, pwd]):
